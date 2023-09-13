ee4fe5a5ebad9aec044f744a1fd3d592057dc4c2
ARUNKUMARMARU <arunsmart1020@gmail.com>
2023-09-13 11:49:10 +0530

    practice added

--- day 16/excersice.js
+++ day 16/excersice.js
@@ -1,1 +1,8 @@
-history
+function greetings(name, callback){
+    setTimeout(function () {
+          callback(`hello! ${name},..`)    }, 2000)
+}
+
+greetings('Arunkumar', function (callback) {
+    console.log(callback);
+});
